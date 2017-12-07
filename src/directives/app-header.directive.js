e92f75248f54309820f90ce138c752181557c681
adrian <adrian@bright-things.com>
2017-12-07 11:53:27 +0000

    Services added

--- src/directives/app-header.directive.js
+++ src/directives/app-header.directive.js
@@ -1,3 +1,4 @@
+
 /*
  * App Header
  */
@@ -5,12 +6,22 @@
 
     var module = angular.module("app-header", []);
 
+    module.controller("HeaderController", ["$log", "SystemService",
+        function ($log, systemService) {
+            $log.debug("HeaderController: starting");
+            var that = this;
+            that.systemService = systemService;
+        }
+    ]);
+
     module.directive("appHeader", [
         function () {
             return {
                 restrict: "E",
                 replace: true,
-                templateUrl: "views/app-header.html"
+                templateUrl: "views/app-header.html",
+                controller: "HeaderController",
+                controllerAs: "headerCtrl"
             };
         }
     ]);
